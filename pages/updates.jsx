f0d4616950f85e40b8d6f49702f2ebbc7632a39b
nandan-varma <nandanvarma.me@gmail.com>
2023-02-01 16:13:03 +0530

    fixed deployment issue

--- pages/updates.jsx
+++ pages/updates.jsx
@@ -1,8 +1,10 @@
 import { Inter } from '@next/font/google'
+import Content from '../components/Content';
 
 export default function Events() {
     return (
       <>
+        <Content/>
         <hr />
       </>
     )
